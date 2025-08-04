504755f292357b92304bf7c90b0bd24b99e8180b
r9988.2588 <r9988.2588@gmail.com>
2025-08-04 04:04:13 +0000

    added dto's

---
+++ src/main/java/com/project/CommunityManagementSystemAPI/dto/profile/ProfileRequest.java
@@ -0,0 +1,29 @@
+package com.project.CommunityManagementSystemAPI.dto.profile;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileRequest {
+    @NotNull
+    private long userId;
+
+    @NotBlank
+    private String username;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String bio;
+}
---
+++ src/main/java/com/project/CommunityManagementSystemAPI/dto/profile/ProfileResponse.java
@@ -0,0 +1,19 @@
+package com.project.CommunityManagementSystemAPI.dto.profile;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileResponse {
+    private Long id;
+    private long userId;
+    private String username;
+    private LocalDate dateOfBirth;
+    private String city;
+    private String bio;
+}
