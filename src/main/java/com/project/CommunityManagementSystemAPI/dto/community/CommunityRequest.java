5d6ca5fa8102a7f88349b717c418bd49db9bfb51
r9988.2588 <r9988.2588@gmail.com>
2025-08-05 03:48:07 +0000

    added community dto's

---
+++ src/main/java/com/project/CommunityManagementSystemAPI/dto/community/CommunityRequest.java
@@ -0,0 +1,20 @@
+package com.project.CommunityManagementSystemAPI.dto.community;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommunityRequest {
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Description cannot be blank")
+    private String description;
+
+    @NotBlank
+    private String ownerUsername;
+}
---
+++ src/main/java/com/project/CommunityManagementSystemAPI/dto/community/CommunityResponse.java
@@ -0,0 +1,15 @@
+package com.project.CommunityManagementSystemAPI.dto.community;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommunityResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String ownerUsername;
+}
