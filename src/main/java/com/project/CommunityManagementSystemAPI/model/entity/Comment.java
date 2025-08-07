a0591fd109658481d078fe7754a7c0a7ce0f78f6
r9988.2588 <r9988.2588@gmail.com>
2025-08-07 05:47:22 +0000

    added comment model

---
+++ src/main/java/com/project/CommunityManagementSystemAPI/model/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.project.CommunityManagementSystemAPI.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Profile author;
+
+    @Column(nullable = false)
+    private String content;
+}
